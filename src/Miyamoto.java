810a44775713b1bcfdad30dd51543c963a9db877
Hasan <hassanali.432an@gmail.com>
2026-03-01 17:41:36 -0500

    Basic project setup

---
+++ src/Miyamoto.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;public class Miyamoto extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        TextArea textArea = new TextArea();
+        textArea.setPrefHeight(100);
+
+        Button helloButton = new Button("Hello");
+        Button goodbyeButton = new Button("Goodbye");
+
+        helloButton.setOnAction(e -> textArea.setText("hello"));
+        goodbyeButton.setOnAction(e -> textArea.clear());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(textArea, helloButton, goodbyeButton);
+
+        Scene scene = new Scene(layout, 300, 200);
+
+        primaryStage.setTitle("Simple JavaFX App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args); // Triggers start()
+    }
+
+}
---
+++ src/com/btm495/miytamoto/objects/Customer.java
@@ -0,0 +1,77 @@
+package com.btm495.miytamoto.objects;
+
+public class Customer {
+
+    private int customerId;
+    private String name;
+    private String address;
+    private int phoneNumber;
+    private String password;
+    private String emailAddress;
+    private int loyaltyProgram;
+
+    public Customer(String name, String address, int phoneNumber, String password, String emailAddress, int loyaltyProgram) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.emailAddress = emailAddress;
+        this.loyaltyProgram = loyaltyProgram;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getLoyaltyProgram() {
+        return loyaltyProgram;
+    }
+
+    public void setLoyaltyProgram(int loyaltyProgram) {
+        this.loyaltyProgram = loyaltyProgram;
+    }
+}
